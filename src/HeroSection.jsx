1507650477c2a8a11ebcf3dda55c969ea55b9900
Charity-Nelima <wa.nelimacharity@gmail.com>
2024-10-11 15:18:05 +0300

    edit navbar and footer

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -10,10 +10,10 @@ import Contact from './components/Contact'
 import Nelima from './assets/Nelima.jpg'
 import pdf from './assets/Charity_Nelima_CV.pdf'
 const navigation = [
-    { name: 'Product', href: '#' },
-    { name: 'Features', href: '#' },
-    { name: 'Marketplace', href: '#' },
-    { name: 'Company', href: '#' },
+    { name: 'About', href: '#' },
+    { name: 'Technologies', href: '#' },
+    { name: 'Experience', href: '#' },
+    { name: 'Projects', href: '#' },
 ]
 
 export default function HeroSection() {
@@ -51,9 +51,7 @@ export default function HeroSection() {
                         ))}
                     </div>
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                            Log in <span aria-hidden="true">&rarr;</span>
-                        </a>
+                     
                     </div>
                 </nav>
                 <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
@@ -90,14 +88,7 @@ export default function HeroSection() {
                                         </a>
                                     ))}
                                 </div>
-                                <div className="py-6">
-                                    <a
-                                        href="#"
-                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                    >
-                                        Log in
-                                    </a>
-                                </div>
+                               
                             </div>
                         </div>
                     </DialogPanel>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,9 @@ export default function Footer() {
                         <div class="flex flex-col items-center justify-between gap-6 sm:flex-row">
                             <div class="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                                 <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/about">About</a>
-                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/projects">Projects</a>
-                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/speaking">Speaking</a>
-                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/uses">Uses</a>
+                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/technologies">Technologies</a>
+                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/speaking">Experiences</a>
+                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/uses">Projects</a>
                             </div>
                             <p class="text-sm text-zinc-400 dark:text-zinc-500">© 2024 Spencer Sharp. All rights reserved.</p>
                         </div>
