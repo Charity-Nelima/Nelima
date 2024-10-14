457080fcfbebee21cb5113a5c03fab1b6e784781
Charity-Nelima <wa.nelimacharity@gmail.com>
2024-10-14 11:04:39 +0300

    complete copywriting for the work experiences section

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -4,81 +4,91 @@ export default function Experiences() {
     return (
         <div>
             <section id='experiences'>
-            <div class="relative px-4 sm:px-8 lg:px-12 items-center justify-center mt-20 ">
-                <div class="mx-auto max-w-2xl lg:max-w-5xl">
-                    <header class="max-w-2xl">
-                        <h1 class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl ">Work Experiences.</h1>
-                        <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.</p></header>
-                    <div class="mt-16 sm:mt-20"><div class="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-                        <div class="flex max-w-3xl flex-col space-y-16">
-                            <article class="md:grid md:grid-cols-4 md:items-baseline">
-                                <div class="md:col-span-3 group relative flex flex-col items-start">
+                <div class="relative px-4 sm:px-8 lg:px-12 items-center justify-center mt-20 ">
+                    <div class="mx-auto max-w-2xl lg:max-w-5xl">
+                        <header class="max-w-2xl">
+                            <h1 class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl ">Work Experiences.</h1>
+                            <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">Here is an organized showcase of my key contributions in programming, and product development, illustrating growth and milestones over time.</p></header>
+                        <div class="mt-16 sm:mt-20"><div class="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+                            <div class="flex max-w-3xl flex-col space-y-16">
+                                <article class="md:grid md:grid-cols-4 md:items-baseline">
+                                    <div class="md:col-span-3 group relative flex flex-col items-start">
 
-                                    <h2 class="text-base font-semibold tracking-tight text-zinc-800 ">
-                                        <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:rounded-2xl "></div>
-                                        <a href="/articles/crafting-a-design-system-for-a-multiplanetary-future">
-                                            <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl">
-                                            </span><span class="relative z-10">Crafting a design system for a multiplanetary future</span></a>
-                                    </h2><time class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400  pl-3.5" datetime="2022-09-05">
-                                        <span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
-                                            <span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
-                                        </span>September 5, 2022</time>
-                                    <p class="relative z-10 mt-2 text-sm text-zinc-600 ">Most companies try to stay ahead of the curve when it comes to visual design, but for Planetaria we needed to create a brand that would still inspire us 100 years from now when humanity has spread across our entire solar system.</p>
-                                    <div aria-hidden="true" class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">Read article
-                                        <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
-                                            <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        </svg>
+                                        <h2 class="text-base font-semibold tracking-tight text-zinc-800 ">
+                                            <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:rounded-2xl "></div>
+                                            <h2>
+                                                <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl">
+                                                </span><span class="relative z-10">Hemllin |Software Developer</span></h2>
+                                            <h2>
+                                                <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-xl ">
+                                                </span><span class="relative z-10 ">Accomplishments</span></h2>
+                                        </h2><time class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400  pl-3.5" datetime="2022-09-05">
+                                            <span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
+                                                <span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
+                                            </span>Sept 9, 2023 - May 10, 2024</time>
+                                        <p class="relative z-10 mt-2 text-sm text-zinc-600 ">● Developed web applications using React and Ruby, ensuring performance and security.<br />
+                                            ● Active in Agile methodologies, fostered team collaboration and timely project delivery.<br />
+                                            ●Collaborated with the design team to align technical and business requirements.<br />
+                                            ● Implement comprehensive testing to maintain code quality and optimize performance.<br></br>
+                                            ● Contributed to impactful software solutions with a dynamic team.</p>
+
                                     </div>
-                                </div>
-                                <time class="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 " datetime="2022-09-05">September 5, 2022</time>
-                            </article>
-                            <article class="md:grid md:grid-cols-4 md:items-baseline">
-                                <div class="md:col-span-3 group relative flex flex-col items-start">
-                                    <h2 class="text-base font-semibold tracking-tight text-zinc-800 ">
-                                        <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
-                                        <a href="/articles/introducing-animaginary"><span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                                            <span class="relative z-10">Introducing Animaginary: High performance web animations</span></a>
-                                    </h2>
-                                    <time class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5" datetime="2022-09-02">
-                                        <span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
-                                            <span class="h-4 w-0.5 rounded-full bg-zinc-200 "></span>
-                                        </span>September 2, 2022</time>
-                                    <p class="relative z-10 mt-2 text-sm text-zinc-600 ">When you’re building a website for a company as ambitious as Planetaria, you need to make an impression. I wanted people to visit our website and see animations that looked more realistic than reality itself.</p>
-                                    <div aria-hidden="true" class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">Read article
-                                        <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
-                                            <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        </svg>
+                                    <time class="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 " datetime="2022-09-05">Sept 9, 2023 - May 10, 2024</time>
+                                </article>
+
+                                <article class="md:grid md:grid-cols-4 md:items-baseline">
+                                    <div class="md:col-span-3 group relative flex flex-col items-start">
+
+                                        <h2 class="text-base font-semibold tracking-tight text-zinc-800 ">
+                                            <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:rounded-2xl "></div>
+                                            <h2>
+                                                <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl">
+                                                </span><span class="relative z-10">KaaKazini |Wordpress Developer</span></h2>
+                                            <h2>
+                                                <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-xl ">
+                                                </span><span class="relative z-10 ">Accomplishments</span></h2>
+                                        </h2><time class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400  pl-3.5" datetime="2022-09-05">
+                                            <span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
+                                                <span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
+                                            </span>June 3, 2022 - May 9, 2023</time>
+                                        <p class="relative z-10 mt-2 text-sm text-zinc-600 ">
+                                            ● Worked with the team to integrate information into the system and work flows.<br/>
+                                            ● Created and executed a strategic plan/ technical plan and made sure the plan aligns with
+                                            the company business objectives.<br/>
+                                            ● Created a cloud database of craftsmen on our website configured correctly with the
+                                            website user interface and a GIS functionality, with automated prompts for the
+                                            craftsmen to complete their profiles, and GIS location.</p>
+
                                     </div>
-                                </div>
-                                <time class="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 " datetime="2022-09-02">September 2, 2022</time>
-                            </article>
-                            <article class="md:grid md:grid-cols-4 md:items-baseline">
-                                <div class="md:col-span-3 group relative flex flex-col items-start">
-                                    <h2 class="text-base font-semibold tracking-tight text-zinc-800">
-                                        <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:-inset-x-6 sm:rounded-2xl "></div>
-                                        <a href="/articles/rewriting-the-cosmos-kernel-in-rust">
-                                            <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                                            <span class="relative z-10">Rewriting the cosmOS kernel in Rust</span>
-                                        </a>
-                                    </h2>
-                                    <time class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 pl-3.5" datetime="2022-07-14">
-                                        <span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
-                                            <span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
-                                        </span>July 14, 2022</time>
-                                    <p class="relative z-10 mt-2 text-sm text-zinc-600 ">When we released the first version of cosmOS last year, it was written in Go. Go is a wonderful programming language, but it’s been a while since I’ve seen an article on the front page of Hacker News about rewriting some important tool in Go and I see articles on there about rewriting things in Rust every single week.</p>
-                                    <div aria-hidden="true" class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">Read article
-                                        <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
-                                            <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        </svg>
+                                    <time class="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 " datetime="2022-09-05">June 3, 2022 - May 9, 2023</time>
+                                </article>
+                                <article class="md:grid md:grid-cols-4 md:items-baseline">
+                                    <div class="md:col-span-3 group relative flex flex-col items-start">
+
+                                        <h2 class="text-base font-semibold tracking-tight text-zinc-800 ">
+                                            <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:rounded-2xl "></div>
+                                            <h2>
+                                                <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl">
+                                                </span><span class="relative z-10">Hemllin |Software Developer</span></h2>
+                                            <h2>
+                                                <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-xl ">
+                                                </span><span class="relative z-10 ">Accomplishments</span></h2>
+                                        </h2><time class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400  pl-3.5" datetime="2022-09-05">
+                                            <span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
+                                                <span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
+                                            </span>Nov 1, 2023 - Dec 30, 2023</time>
+                                        <p class="relative z-10 mt-2 text-sm text-zinc-600 ">● Through teamwork, I managed to acquire diverse skills in developing websites. These
+                                        involved learning different frameworks like React, Node Js and Next Js.</p>
+
                                     </div>
-                                </div>
-                                <time class="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 " datetime="2022-07-14">July 14, 2022</time>
-                            </article>
+                                    <time class="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 " datetime="2022-09-05">Nov 1, 2023 - Dec 30, 2023</time>
+                                </article>
+                               
+                            </div>
+                        </div>
                         </div>
                     </div>
-                    </div>
                 </div>
-            </div>
             </section>
 
         </div>
