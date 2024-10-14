be1c11df46e9a14048806146e4197f292bd57f4c
Charity-Nelima <wa.nelimacharity@gmail.com>
2024-10-14 10:39:17 +0300

    complete editing herosection

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,13 @@ export default function Footer() {
                 <div class="relative px-4 sm:px-8 lg:px-12">
                     <div class="mx-auto max-w-2xl lg:max-w-5xl">
                         <div class="flex flex-col items-center justify-between gap-6 sm:flex-row">
-                            <div class="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="#About">About</a>
-                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="#technologies">Technologies</a>
-                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="#experiences">Experiences</a>
-                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="#projects">Projects</a>
+                            <div class="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-gray-600 ">
+                                <a class="transition hover:text-purple-500 " href="#About">About</a>
+                                <a class="transition hover:text-purple-500 dark:hover:text-purple-400" href="#technologies">Technologies</a>
+                                <a class="transition hover:text-purple-500 dark:hover:text-purple-400" href="#experiences">Experiences</a>
+                                <a class="transition hover:text-purple-500 dark:hover:text-purple-400" href="#projects">Projects</a>
                             </div>
-                            <p class="text-sm text-zinc-400 dark:text-zinc-500">© 2024 Spencer Sharp. All rights reserved.</p>
+                            <p class="text-sm text-zinc-400 dark:text-zinc-500">© 2024 Charity Nelima. All rights reserved.</p>
                         </div>
                         <div class='mt-5'>
                         <p class="text-sm text-zinc-400 dark:text-zinc-500">+254729815051</p>
