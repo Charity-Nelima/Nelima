8a281be7203198faa1940a2a163c1a2b27ecfd17
Charity-Nelima <wa.nelimacharity@gmail.com>
2024-10-16 18:10:20 +0300

    complete the technologies section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ export default function Projects() {
         <div className="relative px-4 sm:px-8 lg:px-12 mt-20">
           <div className="mx-auto max-w-2xl lg:max-w-5xl">
             <header className="max-w-2xl">
-              <h1 className="text-4xl font-bold tracking-tight text-zinc-070 sm:text-5xl ">
+              <h1 className="text-42l font-bold tracking-tight text-zinc-070 sm:text-5xl ">
                 My Projects
               </h1>
               <p className="mt-6 text-base text-zinc-600 ">
