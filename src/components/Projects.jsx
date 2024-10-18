0a9ae6926318f750524732edb8697d9d617cdf90
Charity-Nelima <wa.nelimacharity@gmail.com>
2024-10-18 15:12:10 +0300

    add refactored projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -53,7 +53,6 @@ export default function Projects() {
                   </a>
                 </li>
 
-
                 <li className="group relative flex flex-col items-start">
                   <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 ">
                     <img
@@ -65,30 +64,67 @@ export default function Projects() {
                       data-nimg="1"
                       className="h-8 w-8"
                       src="https://spotlight.tailwindui.com/_next/static/media/starbucks.4a5bd050.svg"
-                      style={{ color: 'transparent' }}
+                      style={{ color: 'transparent' }} // Change to a style object
                     />
                   </div>
                   <h2 className="mt-6 text-base font-semibold text-zinc-800 ">
-                    <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition  sm:-inset-x-6 sm:rounded-2xl"></div>
-                    <a href="http://planetaria.tech">
-                      <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                      <span className="relative z-10">Planetaria</span>
-                    </a>
-                    <a href='https://www.hemllin.com/' aria-hidden="true" class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">Read article
+                    <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:-inset-x-6 sm:rounded-2xl "></div>
+
+                    <span className="relative z-10">Hemllin</span>
+
+                  </h2>
+                  <p className="relative z-10 mt-2 text-sm text-zinc-600 ">
+                    Creating technology to empower civilians to explore space on their own terms.
+                  </p>
+                  <a href="https://www.hemllin.com/" className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-purple-400 dark:text-zinc-400">
+                    <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6 flex-none">
+                      <path d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z" fill="currentColor"></path>
+                    </svg>
+                    <span className="ml-2">planetaria.tech</span>
+
+                  </a>
+                  <a href='https://www.hemllin.com/' aria-hidden="true" class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">Read article
                     <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
                       <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                     </svg>
                   </a>
+                </li>
+
+                <li className="group relative flex flex-col items-start">
+                  <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 ">
+                    <img
+                      alt=""
+                      loading="lazy"
+                      width="32"
+                      height="32"
+                      decoding="async"
+                      data-nimg="1"
+                      className="h-8 w-8"
+                      src="https://spotlight.tailwindui.com/_next/static/media/starbucks.4a5bd050.svg"
+                      style={{ color: 'transparent' }} // Change to a style object
+                    />
+                  </div>
+                  <h2 className="mt-6 text-base font-semibold text-zinc-800 ">
+                    <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:-inset-x-6 sm:rounded-2xl "></div>
+
+                    <span className="relative z-10">Hemllin</span>
+
                   </h2>
                   <p className="relative z-10 mt-2 text-sm text-zinc-600 ">
                     Creating technology to empower civilians to explore space on their own terms.
                   </p>
-                  <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-purple-400 dark:text-zinc-400">
+                  <a href="https://www.hemllin.com/" className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-purple-400 dark:text-zinc-400">
                     <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6 flex-none">
                       <path d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z" fill="currentColor"></path>
                     </svg>
                     <span className="ml-2">planetaria.tech</span>
-                  </p>
+
+                  </a>
+                  <a href='https://www.hemllin.com/' aria-hidden="true" class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">Read article
+                    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
+                      <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                  </a>
                 </li>
 
 
@@ -107,54 +143,27 @@ export default function Projects() {
                     />
                   </div>
                   <h2 className="mt-6 text-base font-semibold text-zinc-800 ">
-                    <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:rounded-2xl "></div>
-                    <a href="http://planetaria.tech">
-                      <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                      <span className="relative z-10">Planetaria</span>
-                    </a>
+                    <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition sm:-inset-x-6 sm:rounded-2xl "></div>
+
+                    <span className="relative z-10">Hemllin</span>
+
                   </h2>
                   <p className="relative z-10 mt-2 text-sm text-zinc-600 ">
                     Creating technology to empower civilians to explore space on their own terms.
                   </p>
-                  <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-purple-400 dark:text-zinc-400">
+                  <a href="https://www.hemllin.com/" className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-purple-400 dark:text-zinc-400">
                     <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6 flex-none">
                       <path d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z" fill="currentColor"></path>
                     </svg>
                     <span className="ml-2">planetaria.tech</span>
-                  </p>
-                </li>
-                <li className="group relative flex flex-col items-start">
-                  <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5">
-                    <img
-                      alt=""
-                      loading="lazy"
-                      width="32"
-                      height="32"
-                      decoding="async"
-                      data-nimg="1"
-                      className="h-8 w-8"
-                      src="https://spotlight.tailwindui.com/_next/static/media/starbucks.4a5bd050.svg"
-                      style={{ color: 'transparent' }} // Change to a style object
-                    />
-                  </div>
-                  <h2 className="mt-6 text-base font-semibold text-zinc-800">
-                    <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 "></div>
-                    <a href="http://planetaria.tech">
-                      <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                      <span className="relative z-10">Planetaria</span>
-                    </a>
-                  </h2>
-                  <p className="relative z-10 mt-2 text-sm text-zinc-600 ">
-                    Creating technology to empower civilians to explore space on their own terms.
-                  </p>
-                  <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-purple-400 dark:text-zinc-400">
-                    <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6 flex-none">
-                      <path d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z" fill="currentColor"></path>
+
+                  </a>
+                  <a href='https://www.hemllin.com/' aria-hidden="true" class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">Read article
+                    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
+                      <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                     </svg>
-                    <span className="ml-2">planetaria.tech</span>
-                  </p>
+                  </a>
                 </li>
-                {/* Repeat other list items similarly */}
               </ul>
             </div>
           </div>
