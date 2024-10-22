b24fc532340da7a09a153eeebdc0a7dee9ee2032
Charity-Nelima <wa.nelimacharity@gmail.com>
2024-10-22 11:05:12 +0300

    general code refactoring

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact() {
             </div>
             <div className="px-4 py-10 sm:px-10 sm:py-16 md:py-20 lg:px-20 lg:py-32">
               <h2
-                className="inline-flex items-center rounded-full px-4 py-1 text-blue-600 ring-1 ring-inset ring-blue-600"
+                className="inline-flex items-center rounded-full px-4 py-1 text-blue-600 ring-1 ring-inset ring-purple-400"
                 id="author-title"
               >
                 <span className="font-mono text-sm" aria-hidden="true">05</span>
@@ -40,7 +40,7 @@ export default function Contact() {
                "" charity.w.nelima@gmail.com"
               </p>
               <p className="mt-8">
-                <a className="inline-flex items-center text-base font-medium tracking-tight text-slate-900" href="#">
+                <a className="inline-flex items-center text-base font-medium tracking-tight text-slate-900" href="https://x.com/CharityNelima2">
                   <svg aria-hidden="true" viewBox="0 0 24 24" className="h-10 w-10 fill-current">
                     <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8132L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z"></path>
                   </svg>
